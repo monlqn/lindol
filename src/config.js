69a226d6efe2cedea6b8291620763d2572f8b197
Monching <moncodes93@gmail.com>
2026-06-08 18:46:12 +0800

    feat: add region config constants

---
+++ src/config.js
@@ -0,0 +1,8 @@
+// Southern Mindanao region settings shared across features.
+export const REGION = {
+  center: [7.08, 126.18],
+  defaultUser: [7.085, 126.052],
+  bbox: { minLat: 4.5, maxLat: 9.5, minLng: 124.0, maxLng: 128.0 },
+  minMagnitude: 2.5,
+  windowDays: 7,
+};
